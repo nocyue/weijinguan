c1c3594e1795f4132c027428ec4da66997d69e8d
nocyue <yueweb@163.com>
2017-02-02 22:22:43 +0800

    nocyue

--- js/main.js
+++ js/main.js
@@ -30,6 +30,31 @@ $(function() {
       }
     });		
 	}
-	
+
+  var OFFSET = 50;
+  // 轮播图触摸
+  $('.carousel').each(function(i, item) {
+    var startX, endX;
+    item.addEventListener('touchstart', function(e) {
+      startX = e.touches[0].clientX;
+      e.preventDefault();
+    });
+    item.addEventListener('touchmove', function(e) {
+      endX = e.touches[0].clientX;
+      e.preventDefault();
+    });
+    item.addEventListener('touchend', function(e) {
+      var offsetX = endX - startX;
+      if (offsetX > OFFSET) {
+        // 上一张
+        $(this).carousel('prev');
+      } else if (offsetX < -OFFSET) {
+        // 上一张
+        $(this).carousel('next');
+      }
+      e.preventDefault();
+    });
+  });	
+
 	$(window).on('resize', resize).trigger('resize');
 });
